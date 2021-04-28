69bad09340b0f5f6488a5c0c1e88becc7d68e4ea
uiissoop <ckq1212@naver.com>
2021-04-28 21:37:58 +0900

    update
    update

--- assets/js/swiper.js
+++ assets/js/swiper.js
@@ -1,4 +1,4 @@
-const slide = new Swiper('.swiper-container', {
+new Swiper('.swiper01', {
   slidesPerView : 'auto', // 한 슬라이드에 보여줄 갯수
   spaceBetween : 42, 
   // 슬라이드 사이 여백
@@ -17,6 +17,32 @@ const slide = new Swiper('.swiper-container', {
       spaceBetween : 46, // 슬라이드 사이 여백
     },
   },
+  
+  })
+
+
+new Swiper('.swiper02', {
+  slidesPerView : '4', // 한 슬라이드에 보여줄 갯수
+  spaceBetween : 15, 
+  // 슬라이드 사이 여백
+  loop : true, // 슬라이드 반복 여부
+  loopAdditionalSlides : 1, // 슬라이드 반복 시 마지막 슬라이드에서 다음 슬라이드가 보여지지 않는 현상 수정
+  pagination : false, // pager 여부
+  navigation: {   // 버튼 사용자 지정
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+  },
+  autoplay: {
+    delay : 5000,
+    disableOnInteraction : false,
+  },
+  // slidesOffsetBefore :  // 슬라이드 시작 부분 여백
+
+  // breakpoints : { // 반응형 설정이 가능 width값으로 조정
+  //   1 : {
+  //     spaceBetween : 15, // 슬라이드 사이 여백
+  //   },
+  // },
   
   })
 
