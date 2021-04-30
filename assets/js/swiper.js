547662cdea78fbb7ba1d0e1131de77bf3d839e05
uiissoop <74228804+uiissoop@users.noreply.github.com>
2021-04-30 18:19:05 +0900

    footer-update
    footer-update

--- assets/js/swiper.js
+++ assets/js/swiper.js
@@ -13,7 +13,6 @@ new Swiper('.swiper01', {
 
   breakpoints : { // 반응형 설정이 가능 width값으로 조정
     1 : {
-  
       spaceBetween : 46, // 슬라이드 사이 여백
     },
   },
@@ -21,79 +20,46 @@ new Swiper('.swiper01', {
   })
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+  new Swiper('.swiper02', {
+    slidesPerView : '4', // 한 슬라이드에 보여줄 갯수
+    spaceBetween : 15, 
+    // 슬라이드 사이 여백
+    loop : true, // 슬라이드 반복 여부
+    loopAdditionalSlides : 1, // 슬라이드 반복 시 마지막 슬라이드에서 다음 슬라이드가 보여지지 않는 현상 수정
+    pagination : false, // pager 여부
+    navigation: {   // 버튼 사용자 지정
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+    autoplay: {
+      delay : 5000,
+      disableOnInteraction : true,
+    },
+    // slidesOffsetBefore :  // 슬라이드 시작 부분 여백
   
-
-new Swiper('.swiper02', {
-  slidesPerView : '4', // 한 슬라이드에 보여줄 갯수
-  spaceBetween : 15, 
-  // 슬라이드 사이 여백
-  loop : true, // 슬라이드 반복 여부
-  loopAdditionalSlides : 1, // 슬라이드 반복 시 마지막 슬라이드에서 다음 슬라이드가 보여지지 않는 현상 수정
-  pagination : false, // pager 여부
-  navigation: {   // 버튼 사용자 지정
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-  },
-  autoplay: {
-    delay : 5000,
-    disableOnInteraction : true,
-  },
-  // slidesOffsetBefore :  // 슬라이드 시작 부분 여백
-
-  // breakpoints : { // 반응형 설정이 가능 width값으로 조정
-  //   1 : {
-  //     spaceBetween : 15, // 슬라이드 사이 여백
-  //   },
-  // },
+    // breakpoints : { // 반응형 설정이 가능 width값으로 조정
+    //   1 : {
+    //     spaceBetween : 15, // 슬라이드 사이 여백
+    //   },
+    // },
+    
+    })
   
-  })
-
-  // var swiper = new Swiper('.swiper-container', {
-  //   slidesPerView: 1,
-  //   spaceBetween: 10,
-  //   breakpoints: {
-  //     '@0.75': {
-  //       slidesPerView: 2,
-  //       spaceBetween: 20,
-  //     },
-  //     '@1.00': {
-  //       slidesPerView: 3,
-  //       spaceBetween: 40,
-  //     },
-  //     '@1.50': {
-  //       slidesPerView: 4,
-  //       spaceBetween: 50,
-  //     },
-  //   }
-  // });
-
-
-  
+    // var swiper = new Swiper('.swiper-container', {
+    //   slidesPerView: 1,
+    //   spaceBetween: 10,
+    //   breakpoints: {
+    //     '@0.75': {
+    //       slidesPerView: 2,
+    //       spaceBetween: 20,
+    //     },
+    //     '@1.00': {
+    //       slidesPerView: 3,
+    //       spaceBetween: 40,
+    //     },
+    //     '@1.50': {
+    //       slidesPerView: 4,
+    //       spaceBetween: 50,
+    //     },
+    //   }
+    // });
